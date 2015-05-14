ddd836305d1bb241eb4009e9be3b702282c13ece
Jono <jono@platform45.com>
2015-05-14 12:50:13 +0200

    :star2: added a stop button and show it when the record button is tapped

--- app/controllers/recorder_controller.rb
+++ app/controllers/recorder_controller.rb
@@ -10,14 +10,25 @@ class RecorderController < UIViewController
     @title.center = [self.view.frame.size.width / 2, 40]
     self.view.addSubview(@title)
 
+    # Main Button position
+    button_position = CGRect.new([(self.view.frame.size.width / 2) - 20, 70], [40, 40])
+
     # Record Button
     @record_button = UIButton.buttonWithType(UIButtonTypeSystem)
-    @record_button.frame = CGRect.new([(self.view.frame.size.width / 2) - 20, 70], [40, 40])
+    @record_button.frame = button_position
     @record_button.layer.cornerRadius = 20
     @record_button.backgroundColor = UIColor.redColor
+    @record_button.addTarget(self, action:"start_recording", forControlEvents:UIControlEventTouchUpInside)
     self.view.addSubview(@record_button)
+
+    # Stop Button
+    @stop_button = UIButton.buttonWithType(UIButtonTypeSystem)
+    @stop_button.frame = button_position
+    @stop_button.backgroundColor = UIColor.blackColor
   end
 
   def start_recording
+    # Show stop button
+    self.view.addSubview(@stop_button)
   end
 end
