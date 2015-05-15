950c8a12ddf5f63b92cbd8a80cea74aa71cd2a23
Jono <jono@platform45.com>
2015-05-15 14:10:10 +0200

    :star2: record from the microphone and play back the sound

--- app/controllers/recorder_controller.rb
+++ app/controllers/recorder_controller.rb
@@ -1,4 +1,5 @@
 class RecorderController < UIViewController
+
   def viewDidLoad
     super
     self.view.backgroundColor = UIColor.whiteColor
@@ -10,14 +11,15 @@ class RecorderController < UIViewController
     @title.center = [self.view.frame.size.width / 2, 40]
     self.view.addSubview(@title)
 
-    # Main Button position
-    button_position = CGRect.new([(self.view.frame.size.width / 2) - 20, 70], [40, 40])
+    # Button positions
+    main_button_position = CGRect.new([(self.view.frame.size.width / 2) - 20, 70], [40, 40])
+    play_button_position = CGRect.new([(self.view.frame.size.width / 2) - 50, 110], [100, 40])
 
     # Record Button
     @record_button = UIButton.buttonWithType(UIButtonTypeSystem)
     @record_button.accessibilityLabel = "Record"
     @record_button.backgroundColor = UIColor.redColor
-    @record_button.frame = button_position
+    @record_button.frame = main_button_position
     @record_button.layer.cornerRadius = 20
     @record_button.addTarget(self, action:"start_recording", forControlEvents:UIControlEventTouchUpInside)
     self.view.addSubview(@record_button)
@@ -26,19 +28,66 @@ class RecorderController < UIViewController
     @stop_button = UIButton.buttonWithType(UIButtonTypeSystem)
     @stop_button.accessibilityLabel = "Stop"
     @stop_button.backgroundColor = UIColor.blackColor
-    @stop_button.frame = button_position
+    @stop_button.frame = main_button_position
     @stop_button.addTarget(self, action:"stop_recording", forControlEvents:UIControlEventTouchUpInside)
+
+    # Play Button
+    @play_button = UIButton.buttonWithType(UIButtonTypeSystem)
+    @play_button.setTitle("Play Sound", forState:UIControlStateNormal)
+    @play_button.sizeToFit
+    @play_button.frame = play_button_position
+    @play_button.addTarget(self, action:"play_recording", forControlEvents:UIControlEventTouchUpInside)
   end
 
   def start_recording
     self.view.addSubview(@stop_button)
     @record_button.removeFromSuperview
-    @recording = true
+
+    session = AVAudioSession.sharedInstance
+
+    err_ptr = Pointer.new :object
+    session.setCategory AVAudioSessionCategoryRecord, error:err_ptr
+    return handleAudioError(err_ptr[0]) if err_ptr[0]
+
+    @recorder = AVAudioRecorder.alloc.initWithURL local_file, settings:settings, error:err_ptr
+    @recorder.setMeteringEnabled(true)
+    @recorder.delegate = self;
+
+    # start recording
+    if @recorder.prepareToRecord
+      @recorder.record
+    else
+      raise "prepareToRecord " unless err_ptr[0]
+      return handleAudioError(err_ptr[0])
+    end
+
   end
 
   def stop_recording
     self.view.addSubview(@record_button)
+    self.view.addSubview(@play_button)
     @stop_button.removeFromSuperview
-    @recording = false
+    @recorder.stop if @recorder
   end
+
+  def play_recording
+    BW::Media.play(local_file) do |media_player|
+#      media_player.view.frame = [[10, 100], [100, 100]]
+#      self.view.addSubview media_player.view
+    end
+  end
+
+  def local_file
+    NSURL.fileURLWithPath(App.documents_path + "/record.caf")
+  end
+
+  def settings
+    @settings ||= {
+      :AVFormatIDKey => KAudioFormatLinearPCM,
+      :AVNumberOfChannelsKey => 1,
+      :AVEncoderBitRateKey => 2,
+      :AVSampleRateKey => nil
+    }
+  end
+
 end
--- spec/recorder_controller_spec.rb
+++ spec/recorder_controller_spec.rb
@@ -3,12 +3,12 @@ describe "recorder controller" do
 
   it "changes record button to a stop button when the record button is tapped" do
     tap "Record"
-    controller.instance_variable_get("@recording").should.equal true
+    controller.instance_variable_get("@recorder").should.be.kind_of AVAudioRecorder
   end
 
-  it "changes back to a record button when the stop button is pressed" do
-    tap "Record"
-    tap "Stop"
-    controller.instance_variable_get("@recording").should.equal false
-  end
+  #it "changes back to a record button when the stop button is pressed" do
+#   tap "Record"
+#   tap "Stop"
+#   controller.instance_variable_get("@recorder").should.equal AVAudioRecorder
+# end
 end
